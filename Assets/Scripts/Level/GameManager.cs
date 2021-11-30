c12ea8fe4915eaa91cfbd27a34640f5f176b2f4d
floriandjomain <45943329+floriandjomain@users.noreply.github.com>
2021-11-30 15:45:23 +0100

    Separated GameManager
    - PlayerManager creation
    - TileManager creation

--- Assets/Scripts/Level/GameManager.cs
+++ Assets/Scripts/Level/GameManager.cs
@@ -1,113 +1,33 @@
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using Packages.Rider.Editor.UnitTesting;
 using UnityEngine;
 
 namespace Level
 {
     public class GameManager : MonoBehaviour
     {
-        [SerializeField] private Tile[][] Tiles;
-
-        [SerializeField] private Dictionary<string, Player> Players;
-        [SerializeField] private Dictionary<string, Movement> Movements;
-
+        [SerializeField] private TileManager _tileManager;
+        [SerializeField] private PlayerManager _playerManager;
         [SerializeField] private GameSettings _gameSettings;
         
         public void PlayTurn()
         {
-            DamageTiles();
-            CompileMovements();
-            ApplyMovements();
-            Falls();
-            ResetMovements();
-        }
-
-        private void ApplyMovements()
-        {
-            List<string> conflictedPlayers = new List<string>();
-
-            foreach (string p1 in Players.Keys)
-            {
-                foreach (string p2 in Players.Keys)
-                {
-                    if (p1 == p2 || Players[p1].GetNextPos() != Players[p2].GetNextPos()) continue;
-                    
-                    if(!conflictedPlayers.Contains(p1))
-                        conflictedPlayers.Add(p1);
-                    if(!conflictedPlayers.Contains(p2))
-                        conflictedPlayers.Add(p2);
-                }
-            }
-
-            foreach (string p in Players.Keys)
-            {
-                if(!conflictedPlayers.Contains(p))
-                    Players[p].ApplyMovement();
-            }
-        }
-
-        private void DamageTiles()
-        {
-            Vector2Int pos;
-
-            foreach (string p in Players.Keys)
-            {
-                if (Players[p].IsAlive())
-                {
-                    pos = Players[p].GetPos();
-                    Tiles[pos.x][pos.y].Damage();
-                }
-            }
-        }
-
-        private void Falls()
-        {
-            Vector2Int pos;
-
-            foreach (string p in Players.Keys)
-            {
-                if (Players[p].IsAlive())
-                {
-                    pos = Players[p].GetPos();
-                    
-                    if (Tiles[pos.x][pos.y])
-                        Players[p].Fall();
-                }
-            }
-        }
-
-        private void CompileMovements()
-        {
-            foreach (string p in Players.Keys)
-            {
-                CompileMovement(Players[p], Movements[p]);
-            }
-        }
-
-        private void CompileMovement(Player player, Movement movement)
-        {
-            Vector2Int pos = player.GetPos() + MovementManager.GetVector(movement);
-
-            if (pos.x < 0 || pos.y < 0 || pos.x >= Tiles.Length || pos.y >= Tiles[0].Length) return;
-
-            if (!Tiles[pos.x][pos.y].isActiveAndEnabled) return;
-
-            player.SetNextPos(pos);
+            _tileManager.DamageTiles(_playerManager.GetPlayers());
+            _playerManager.Turn(_tileManager.GetTiles());
         }
 
         public void SetMovement(string player, Movement move)
         {
-            Movements[player] = move;
+            _playerManager.SetMovement(player, move);
         }
 
-        private void ResetMovements()
+        public bool AddPlayer(string playerPseudo)
         {
-            foreach (string p in Movements.Keys)
-            {
-                Movements[p] = Movement.None;
-            }
+            if (_playerManager.GetCurrentPlayerNumber() >= _gameSettings.MaxPlayerNumber) return false;
+            
+            _playerManager.AddPlayer(playerPseudo);
+            return true;
         }
+
+        public void RemovePlayer(string playerPseudo) => _playerManager.RemovePlayer(playerPseudo);
     }
 }
---
+++ Assets/Scripts/Level/TileManager.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level
+{
+    public class TileManager : ScriptableObject
+    {
+        [SerializeField] private Tile[][] Tiles;
+
+        public void SetSize()
+        {
+            
+        }
+
+        public Tile[][] GetTiles() => Tiles;
+
+        public void DamageTiles(Dictionary<string, Player> players)
+        {
+            Vector2Int pos;
+
+            foreach (string p in players.Keys)
+            {
+                if (players[p].IsAlive())
+                {
+                    pos = players[p].GetPos();
+                    Tiles[pos.x][pos.y].Damage();
+                }
+            }
+        }
+    }
+}
