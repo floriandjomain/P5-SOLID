e5288ee2c6f9bb233226be72cffdfe992cb88eaf
Luxifer696 <38383550+Luxifer696@users.noreply.github.com>
2021-12-06 11:40:00 +0100

    Little Update

--- Assets/Scripts/Scene/SceneLoader.cs
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -6,14 +6,16 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField]
-    List<Object> SceneList;
+    private List<Object> SceneList;
 
     void Awake()
     {
         foreach(var scene in SceneList)
         {
+            //Debug.Log(SceneManager.GetActiveScene().name);
             if (scene.name != SceneManager.GetActiveScene().name)
             {
+                //Debug.Log(SceneManager.GetActiveScene().name);
                 //SceneManager.LoadScene(scene.name, LoadSceneMode.Additive);
             }
         }
