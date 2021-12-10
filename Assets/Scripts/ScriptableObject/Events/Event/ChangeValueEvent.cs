bae09142136fd16b852699882d01aea78a2dc7e9
Christophe Fracheboud <cfracheboud@gaming.tech>
2021-12-10 23:38:43 +0100

    Make an event system
    Dont work but i dont know why

---
+++ Assets/Scripts/ScriptableObject/Events/Event/ChangeValueEvent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Events/ChangeValue")]
+
+public class ChangeValueEvent : GameEvent
+{ }
