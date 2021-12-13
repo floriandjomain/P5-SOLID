b712a5979fcbd6b26ffb7b3a954aebcc9ae7fec2
Christophe Fracheboud <cfracheboud@gaming.tech>
2021-12-13 22:04:35 +0100

    Factorise code
    Make InputCommand, a parent of KeyBoard command and Twitch Command

--- Assets/Scripts/ScriptableObject/Keyboard/KeyboardCommand.cs
+++ Assets/Scripts/ScriptableObject/Keyboard/KeyboardCommand.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [CreateAssetMenu(menuName = "Command/Keyboard")]
-public class KeyboardCommand : ScriptableObject
+public class KeyboardCommand : InputCommand
 {
-    public enum CommandType { Lobby, Game }
-
     [SerializeField] private string _name;
     private KeyCode _command;
-    [SerializeField] private CommandType _type;
-    [SerializeField] private UnityEvent<string> _onCommandFound;
 
     public string CommandName { get => _name; }
     public KeyCode Command { get => _command; set => _command = value; }
-    public CommandType Type { get => _type; }
-    public UnityEvent<string> OnCommandFound { get => _onCommandFound; }
+
 }
